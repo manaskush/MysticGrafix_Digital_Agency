4c0c6ee30695b7ce6699f2575aab778741b7ab0c
ManasKush <manaskush311@gmail.com>
2025-01-07 19:40:32 +0530

    as

--- app/website-design.tsx
+++ app/website-design.tsx
@@ -5,12 +5,12 @@ const Project = () => {
     <div className="p-4 mx-auto relative z-10 w-full pt-10 md:pt-20 px-2">
       
       <div className="text-4xl md:text-7xl text-center bg-clip-text text-transparent bg-gradient-to-b from-blue-500 to-neutral-50 bg-opacity-50">
-        Website Design <br /> that works
+        Our Projects
       </div>
 
-      <p className="mt-4 mb-8 text-lg font-normal text-neutral-300 max-w-lg text-center mx-auto px-4">
+      {/* <p className="mt-4 mb-8 text-lg font-normal text-neutral-300 max-w-lg text-center mx-auto px-4">
         Creating, designing and developing websites that work for your business.
-      </p>
+      </p> */}
 
       <div className="flex flex-col items-center">
         <ProjectCard />
